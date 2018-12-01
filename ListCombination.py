adc471009d631f1caafd140c21faf2b3c8c8837f
Felipe <lipe.oli.mo@hotmail.com>
2018-12-01 18:45:27 -0200

    Update ListCombination.py

--- ListCombination.py
+++ ListCombination.py
@@ -1,22 +1,17 @@
-# from itertools import combinations
+from multimethod import multimethod
 
 
 class CombinationList:
     def __init__(self, items):
         self.list = items
         self.listItemsComb = [[]]
-        
-        '''
-        for i in range(len(items)):
-            for item in list(combinations(items, i+1)):
-                self.listItemsComb.append(list(item))
-        '''
 
         for item in items:
-            lenght = len(self.listItemsComb)  # preciso salver o tamanho da lista de combinacoes para nao ter conflito
+            lenght = len(self.listItemsComb)  # preciso salvar o tamanho da lista de combinacoes para nao ter conflito
             for i in range(lenght):
                 self.listItemsComb.append(self.listItemsComb[i][:] + [item])  # faco a combinacao com as ja feitas antes
 
+    @multimethod(object, int)
     def which_sum_equals_to(self, value):
         possibilities = []
 
@@ -26,8 +21,38 @@ class CombinationList:
 
         return possibilities
 
+    @multimethod(object, float)
+    def which_sum_equals_to(self, value):
+        possibilities = []
+
+        for possibilitie in self.listItemsComb:
+            if len(possibilitie) > 0 and sum(possibilitie) == value:
+                possibilities.append(possibilitie)
+
+        return possibilities
+
+    @multimethod(object, int, int)
+    def which_sum_equals_to(self, value, n):
+        possibilities = []
+
+        for possibilitie in self.listItemsComb:
+            if len(possibilitie) == n and sum(possibilitie) == value:
+                possibilities.append(possibilitie)
+
+        return possibilities
+
+    @multimethod(object, float, int)
+    def which_sum_equals_to(self, value, n):
+        possibilities = []
+
+        for possibilitie in self.listItemsComb:
+            if len(possibilitie) == n and sum(possibilitie) == value:
+                possibilities.append(possibilitie)
+
+        return possibilities
+
 
 lista = CombinationList([-25, -10, -7, -3, 2, 4, 8, 10])
 
 print(lista.listItemsComb)
-print(lista.which_sum_equals_to(0, 3))
+print(lista.which_sum_equals_to(0))
