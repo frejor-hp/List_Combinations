f62a92f93a5eb08f1ac3d9139c3ef010f95c0cd4
Felipe <lipe.oli.mo@hotmail.com>
2018-12-02 15:32:55 -0200

    Update ListCombination.py

--- ListCombination.py
+++ ListCombination.py
@@ -12,15 +12,6 @@ class CombinationList:
                 self.listItemsComb.append(self.listItemsComb[i][:] + [item])  # faco a combinacao com as ja feitas antes
 
     @multimethod(object, int)
-    def which_sum_equals_to(self, value):
-        possibilities = []
-
-        for possibilitie in self.listItemsComb:
-            if len(possibilitie) > 0 and sum(possibilitie) == value:
-                possibilities.append(possibilitie)
-
-        return possibilities
-
     @multimethod(object, float)
     def which_sum_equals_to(self, value):
         possibilities = []
@@ -32,15 +23,6 @@ class CombinationList:
         return possibilities
 
     @multimethod(object, int, int)
-    def which_sum_equals_to(self, value, n):
-        possibilities = []
-
-        for possibilitie in self.listItemsComb:
-            if len(possibilitie) == n and sum(possibilitie) == value:
-                possibilities.append(possibilitie)
-
-        return possibilities
-
     @multimethod(object, float, int)
     def which_sum_equals_to(self, value, n):
         possibilities = []
@@ -52,7 +34,7 @@ class CombinationList:
         return possibilities
 
 
-lista = CombinationList([-25, -10, -7, -3, 2, 4, 8, 10])
+lista = CombinationList([-25, -10, -7, -3, 2, 4, 8, 10.5])
 
 print(lista.listItemsComb)
-print(lista.which_sum_equals_to(0))
+print(lista.which_sum_equals_to(0, 3))
