1229aed72e2affbfd1e5c9e8cff129f47eb400b7
Felipe <lipe.oli.mo@hotmail.com>
2018-12-02 17:22:20 -0200

    Update ListCombination.py

--- ListCombination.py
+++ ListCombination.py
@@ -1,28 +1,24 @@
-# from itertools import combinations
-
-
 class CombinationList:
     def __init__(self, items):
         self.list = items
         self.listItemsComb = [[]]
-        
-        '''
-        for i in range(len(items)):
-            for item in list(combinations(items, i+1)):
-                self.listItemsComb.append(list(item))
-        '''
 
         for item in items:
             lenght = len(self.listItemsComb)  # preciso salver o tamanho da lista de combinacoes para nao ter conflito
             for i in range(lenght):
                 self.listItemsComb.append(self.listItemsComb[i][:] + [item])  # faco a combinacao com as ja feitas antes
 
-    def which_sum_equals_to(self, value):
+    def which_sum_equals_to(self, value, n=0):
         possibilities = []
 
-        for possibilitie in self.listItemsComb:
-            if len(possibilitie) > 0 and sum(possibilitie) == value:
-                possibilities.append(possibilitie)
+        if n > 0:
+            for possibilitie in self.listItemsComb:
+                if len(possibilitie) == n and sum(possibilitie) == value:
+                    possibilities.append(possibilitie)
+        else:
+            for possibilitie in self.listItemsComb:
+                if len(possibilitie) > 0 and sum(possibilitie) == value:
+                    possibilities.append(possibilitie)
 
         return possibilities
 
@@ -30,4 +26,4 @@ class CombinationList:
 lista = CombinationList([-25, -10, -7, -3, 2, 4, 8, 10])
 
 print(lista.listItemsComb)
-print(lista.which_sum_equals_to(0, 3))
+print(lista.which_sum_equals_to(0))
