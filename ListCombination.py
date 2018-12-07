932ae2091831d64223f90084d7325a72a4ce76b1
Felipe <lipe.oli.mo@hotmail.com>
2018-12-07 16:39:41 -0200

    Update ListCombination.py

--- ListCombination.py
+++ ListCombination.py
@@ -11,6 +11,13 @@ class CombinationList:
             for i in range(lenght):
                 self.listItemsComb.append(self.listItemsComb[i][:] + [item])  # faco a combinacao com as ja feitas antes
 
+        '''
+        usando list comprehension
+        
+        for item in items:
+            self.listItemsComb += [self.listItemsComb[i][:] + [item] for i in range(len(self.listItemsComb))]
+        '''
+        
     @multimethod(object, int)
     @multimethod(object, float)
     def which_sum_equals_to(self, value):
