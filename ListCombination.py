b8e92feabd940058a29370d590d1a24105df79c2
Felipe <lipe.oli.mo@hotmail.com>
2018-12-18 16:52:02 -0200

    Update ListCombination.py

--- ListCombination.py
+++ ListCombination.py
@@ -1,26 +1,26 @@
 class CombinationList:
     def __init__(self, items):
-        self.list = items
-        self.listItemsComb = [[]]
+        self.items = items
+        self.combinations = [[]]
 
         for item in items:
-            lenght = len(self.listItemsComb)  # preciso salver o tamanho da lista de combinacoes para nao ter conflito
+            lenght = len(self.combinations)  # preciso salver o tamanho da lista de combinacoes para nao ter conflito
             for i in range(lenght):
-                self.listItemsComb.append(self.listItemsComb[i][:] + [item])  # faco a combinacao com as ja feitas antes
+                self.combinations.append(self.combinations[i][:] + [item])  # faco a combinacao com as ja feitas antes
 
         '''
-        usando list comprehension
+        usando items comprehension
 
         for item in items:
-            self.listItemsComb += [self.listItemsComb[i][:] + [item] for i in range(len(self.listItemsComb))]
+            self.combinations += [self.combinations[i][:] + [item] for i in range(len(self.combinations))]
         '''
 
     def which_sum_equals_to(self, value, *n):
         if len(n) > 0:
-            possibilities = [possibilite for possibilite in self.listItemsComb
+            possibilities = [possibilite for possibilite in self.combinations
                              if len(possibilite) in n and sum(possibilite) == value]
         else:
-            possibilities = [possibilite for possibilite in self.listItemsComb
+            possibilities = [possibilite for possibilite in self.combinations
                              if len(possibilite) > 0 and sum(possibilite) == value]
 
         return possibilities
@@ -28,5 +28,5 @@ class CombinationList:
 
 lista = CombinationList([-25, -10, -7, -3, 2, 4, 8, 10])
 
-print(lista.listItemsComb)
+print(lista.combinations)
 print(lista.which_sum_equals_to(0, 2, 3))
